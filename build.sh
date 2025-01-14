818f79dde10c9a76cb84ce08d987d78f16c1db7b
Lucas Albers <lucas.b.albers@gmail.com>
2025-01-14 09:59:45 -0700

    sure enable attestation

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ IMAGE_NAME=lalberslz4/duckdns
 PLATFORMS=linux/amd64,linux/arm64
 BUILDKIT_COMPRESSION=zstd
 BUILDKIT_COMPRESSION_LEVEL=9
-docker buildx build --platform $PLATFORMS --tag $IMAGE_NAME:latest --push .
+docker buildx build --platform $PLATFORMS --tag $IMAGE_NAME:latest --provenance=mode=max --push .
